59fe957e32de02de5ff977cb3a46e4c82a5f0b64
viciousmelicious <viciousmelicious@Melissas-MacBook-Pro.local>
2014-10-14 18:35:21 -0400

    Updated image gallery

--- Slideshow_Example/js/main.js
+++ Slideshow_Example/js/main.js
@@ -7,23 +7,20 @@ function changePicture(whichNum) {
 	currentImage = whichNum;
 }
 
-function nextImage(whichNum) {								//declare function(whichNum)
+function nextImage() {										//declare function(whichNum)
 	var img = document.getElementById('mainImage');			//declare variable img = get mainImage element from DOM, also tried to active gEBI to 'next button' to affect that element and it also broke the code...argh!
 	if (currentImage>images.length){						//if currentImage is greater than the lenght of images array
 		currentImage = 0;									//take currentImage back to 0
 	}
 	currentImage++;											//if not, add one on each sequence in array
-	img.setAttribute('src', images[whichNum]);				//set the attribue of img to the src of the array and each number in images array
-	currentImage = whichNum;								//current image is whichNum function, i tried changing to currenImage = nextImage or nextImage() and broke the code. I'm stumped!!
-
+	img.setAttribute('src', images[currentImage]);				//set the attribue of img to the src of the array and each number in images array
 }
 
-function prevImage(whichNum) {								//declare function(whichNum)
+function prevImage() {										//declare function(whichNum)
 	var img = document.getElementById('mainImage');			//declare variable img = get mainImage Element
 	if (currentImage = -1) {								//
 		currentImage = 0;
 	}
-	img.setAttribute('src', images[whichNum]);
-	currentImage = whichNum;
+	img.setAttribute('src', images[currentImage]);
 }
 
